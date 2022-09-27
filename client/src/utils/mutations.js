4b82c3f5c66939ed2fb93cf4e1bf4fb8d592904d
MosNes <7hekarl@gmail.com>
2022-09-27 12:16:29 -0400

    added queries and mutations placeholders in utils, added utils/auth.js"

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,6 @@
+//collects all GraphQL mutations that change data in the database
+//these mutations are used by the useMutation hook in individual components to make GraphQL queries to the DB
+import { gql } from "@apollo/client";
+
+//ADD ALL CLIENT-SIDE GRAPHQL MUTATIONS HERE
+
