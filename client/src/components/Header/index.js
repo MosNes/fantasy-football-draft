d94fe7082ac3bb5a0d5601add79ace774587c1e7
mackgabe <mackenzieabe@gmail.com>
2022-09-27 20:03:32 -0400

    added some header nav

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,5 +1,6 @@
 //Header component
 import React from 'react';
+//************import bootstrap and react-bootstrap*****
 import { Link } from 'react-router-dom';
 import Auth from '../../utils/auth';
 
@@ -12,10 +13,31 @@ const Header = () => {
     }
 
     return (
-        <header>
-            Header placeholder
+        <header className="bg-secondary mb-4 py-2 flex-row align-center">
+            <div className="container flex-row justify-space-between-lg justify-center align-center">
+                <Link to="/">
+                    <h1>Yay Sports</h1>
+                </Link>
+
+                <nav className="text-center">
+                    {Auth.loggedIn() ? (
+                        <>
+                            <Link to="/home">Home</Link>
+                            <a href="/" onClick={logout}>
+                                Logout
+                            </a>
+                        </>
+                    ) : (
+                        <>
+                            <Link to="/dashboard">Dashboard</Link>
+                            <Link to="/login">Login</Link>
+                            <Link to="/signup">Signup</Link>
+                        </>
+                    )}
+                </nav>
+            </div>
         </header>
-    )
-}
+    );
+};
 
 export default Header;
