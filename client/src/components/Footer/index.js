c2ff0d3dd8635d8dd579a693045f2bb69b455b39
mackgabe <mackenzieabe@gmail.com>
2022-09-27 20:57:59 -0400

    added contact, follow and contributor content to footer component

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,12 +1,44 @@
 //Footer component
 import React from 'react';
+import * as FaIcons from 'react-icons/fa';
 
 const Footer = () => {
   return (
-    <footer>
-      Footer Placeholder
-    </footer>
-  );
-};
+    <section>
+      <div className="footer-container">
+        <h2> Contact Us </h2>
+        <a href="mailto:yaysportsdraft@gmail.com">Email</a>
+      </div>
+
+      <div className="footer-container">
+        <h2> Follow Us </h2>
+        <a href="www.twitter/yaysportsdraft"
+          className="twitter">
+          <FaIcons.FaTwitter size="30px" />
+        </a>
+        <a href="https://github.com/MosNes/fantasy-football-draft.git"
+          className="github">
+          <FaIcons.FaGithub size="30px" />
+        </a>
+        <a href="https://www.instagram.com/yaysportsdraft/"
+          className="instagram">
+          <FaIcons.FaInstagram size="30px" />
+        </a>
+      </div>
+
+      <div className="footer-container">
+        <h2> Contributors </h2>
+        <ul>
+          <li>Cody Cooper</li>
+          <li>Dejuan Strong</li>
+          <li>Mackenzie Abe</li>
+          <li>McKinley Faustin</li>
+          <li>Moses Nester</li>
+        </ul>
+      </div>
+
+</section>
+  )
+}
 
 export default Footer;
