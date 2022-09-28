24c0c38a4315bb7b10daada32198d4e079479385
mackgabe <mackenzieabe@gmail.com>
2022-09-28 15:45:15 -0400

    added some types to type def schema

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -2,8 +2,40 @@
 const { gql } = require('apollo-server-express');
 
 //---------------CREATE TYPEDEFS-----------------------------------
-const typeDefs = gql`
 
+//should first name/last name in model and here just be a username? or do we want first name/last name?
+const typeDefs = gql`
+type User {
+    _id: ID
+    firstName: String
+    lastName: String
+    email: String
+    password: String
+    order: [Order]
+}
+type Player {
+    _id: ID
+    firstName: String
+    lastName: String
+    team_Id: [Team]
+    position: String
+}
+type Team {
+    _id: ID
+    name: String
+    league_Id: [League]
+}
+type League {
+    _id: ID
+    name: String
+}
+type Auth {
+    token: ID!
+    user: User
+}
+type Query {
+    
+}
 `;
 
 //export typeDefs
