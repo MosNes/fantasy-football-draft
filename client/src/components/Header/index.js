73775cd20a89e36ec6f929a29f5b74826d2feba8
mackgabe <mackenzieabe@gmail.com>
2022-09-29 20:57:18 -0400

    trying different bootstrap ideas for navbar component

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,43 +1,116 @@
 //Header component
 import React from 'react';
 //************import bootstrap and react-bootstrap*****
-import { Link } from 'react-router-dom';
 import Auth from '../../utils/auth';
+import Button from 'react-bootstrap/Button';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import{BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import Home from '../../pages/Home';
+import Dashboard from '../../pages/Dashboard';
+import Login from '../../pages/Login';
+import Signup from '../../pages/Signup';
 
-const Header = () => {
+export default class Header extends Component {
+    render() { 
+return (
+        <Router>
+        <div>
 
-    //logout function
-    const logout = event => {
-        event.preventDefault();
-        Auth.logout();
-    }
+            <Navbar bg="dark" variant={"dark"} expand="lg">
+                <Navbar.Brand href="#">Navbar Demo Arjun Codes</Navbar.Brand>
+                <Navbar.Toggle aria-controls="navbarScroll" />
+                <Navbar.Collapse id="navbarScroll">
+                    <Nav
+                        className="mr-auto my-2 my-lg-0"
+                        style={{ maxHeight: '100px' }}
+                        navbarScroll
+                    >
+                        <Nav.Link as={Link} to="/home">Home</Nav.Link>
+                        <Nav.Link as={Link} to="/about">About</Nav.Link>
+                        <Nav.Link as={Link} to="/contact">Contact</Nav.Link>
 
-    return (
-        <header className="bg-secondary mb-4 py-2 flex-row align-center">
-            <div className="container flex-row justify-space-between-lg justify-center align-center">
-                <Link to="/">
-                    <h1>Yay Sports</h1>
-                </Link>
+                    </Nav>
 
-                <nav className="text-center">
-                    {Auth.loggedIn() ? (
-                        <>
-                            <Link to="/home">Home</Link>
-                            <a href="/" onClick={logout}>
-                                Logout
-                            </a>
-                        </>
-                    ) : (
-                        <>
-                            <Link to="/dashboard">Dashboard</Link>
-                            <Link to="/login">Login</Link>
-                            <Link to="/signup">Signup</Link>
-                        </>
-                    )}
-                </nav>
-            </div>
-        </header>
-    );
-};
+                </Navbar.Collapse>
+            </Navbar>
+        </div>
+        <div>
+            <Switch>
+                <Route path="/about">
+                    <About />
+                </Route>
+                <Route path="/contact">
+                    <Contact />
+                </Route>
+                <Route path="/">
+                    <Home />
+                </Route>
+            </Switch>
+        </div>
+    </Router>
+)
+}
+} 
+// const Header = () => {
 
-export default Header;
+//     //logout function
+//     const logout = event => {
+//         event.preventDefault();
+//         Auth.logout();
+//     }
+
+    //     <Navbar bg="light" expand="lg">
+    //     <Container>
+    //       <Navbar.Brand to="/">Fantasy Football</Navbar.Brand>
+    //       <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    //       <Navbar.Collapse id="basic-navbar-nav">
+    //         <Nav className="me-auto">
+    //         {Auth.loggedIn() ? (
+    //             <>
+    //           <Nav.Link to="/home">Home</Nav.Link>
+    //           <a href="/" onClick={logout}>
+    //           Logout
+    //       </a>
+    //       </>
+    //       ) : (
+    //         <Nav className="me-auto">
+    //           <Nav.Link to="/dashboard">Dashboard</Nav.Link>
+    //           <Nav.Link  to="/login">Login</Nav.Link>
+    //           <Nav.Link to="/signup">Signup</Nav.Link>
+
+         
+    //         </Nav>
+    //       </Navbar.Collapse>
+    //     </Container>
+    //   </Navbar>
+
+        // <header className="bg-secondary mb-4 py-2 flex-row align-center">
+        //     <div className="container flex-row justify-space-between-lg justify-center align-center">
+        //         <Link to="/">
+        //             <h1>Fantasy Football</h1>
+        //         </Link>
+
+        //         <nav className="text-center">
+        //             {Auth.loggedIn() ? (
+        //                 <>
+        //                     <Link to="/home">Home</Link>
+        //                     <a href="/" onClick={logout}>
+        //                         Logout
+        //                     </a>
+        //                 </>
+        //             ) : (
+        //                 <>
+        //                     <Link to="/dashboard">Dashboard</Link>
+        //                     <Link to="/login">Login</Link>
+        //                     <Link to="/signup">Signup</Link>
+        //                 </>
+        //             )}
+        //         </nav>
+        //     </div>
+        // </header>
+//     );
+// };
+
+// export default Header;
