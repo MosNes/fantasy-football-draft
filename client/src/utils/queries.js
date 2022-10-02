03600a470b782ef450438fa494334c3adad0f4e4
MosNes <7hekarl@gmail.com>
2022-10-02 15:28:11 -0400

    added GET_USERS GET_USER and ME to client side queries.js

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,3 +1,57 @@
 import { gql } from '@apollo/client';
 
 // ADD ALL CLIENT-SIDE GRAPHQL QUERIES HERE
+export const GET_USERS = gql`
+    query getUsers {
+        getUsers {
+            username
+            firstName
+            lastName
+            email
+        }
+    }
+`;
+
+export const GET_USER = gql`
+    query getUser($email: String!) {
+        getUser(email: $email) {
+        _id
+        username
+        firstName
+        lastName
+        email
+        league_id
+        teams {
+            name
+            league_id {
+            _id
+            }
+        }
+        }
+    }
+`;
+
+export const ME = gql`
+    query me {
+        me {
+            _id
+            username
+            email
+            firstName
+            lastName
+            league_id
+            teams {
+                name
+                players {
+                    _id
+                    name
+                    drafted
+                }
+                league_id {
+                    _id
+                    name
+                }
+            }
+        }
+    }
+`;
