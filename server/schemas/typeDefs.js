ad7dda51710d450b9a0064dd694a9eebed5b77d6
MosNes <7hekarl@gmail.com>
2022-10-06 09:53:27 -0400

    removed comments from typeDefs.js to test

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,9 +1,4 @@
-//import the graphQL tagged template function
 const { gql } = require('apollo-server-express');
-
-//---------------CREATE TYPEDEFS-----------------------------------
-
-//should first name/last name in model and here just be a username? or do we want first name/last name?
 const typeDefs = gql`
 type User {
     _id: ID
@@ -70,6 +65,4 @@ type Mutation {
     login(email: String!, password: String!): Auth
 }
 `;
-
-//export typeDefs
 module.exports = typeDefs;
