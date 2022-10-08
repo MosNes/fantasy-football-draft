b610a2aa4e1749aba3112b80d30b2544fc7a7d33
MosNes <7hekarl@gmail.com>
2022-10-08 18:33:14 -0400

    added predashboard

---
+++ client/src/pages/PreDashboard.js
@@ -0,0 +1,48 @@
+//Wrapper for the Dashboard Page
+
+//checks to see if the user has a league ID. If League ID is null, it renders the Create League form. If League ID is not null, it renders the Dashboard
+
+import React from 'react';
+import { Container, Row } from 'react-bootstrap';
+
+import { useQuery } from '@apollo/client';
+import { ME } from '../utils/queries';
+
+import Dashboard from './Dashboard';
+import LeagueForm from '../components/LeagueForm';
+import JoinLeague from '../components/JoinLeague';
+
+const PreDashboard = () => {
+
+    const { data: { me: { league_id: leagueId, _id: userId, username } = {} } = {}, loading } = useQuery(ME);
+
+    if (loading) {
+        return (
+            <div className='text-white'>LOADING!</div>
+        )
+    }
+
+    if (leagueId === null) {
+        return (
+            <Container>
+                <Row>
+                    <div className='text-white'>YOU NEED TO CREATE OR JOIN A LEAGUE!</div>
+                    <LeagueForm />
+                </Row>
+                <Row>
+                    <JoinLeague />
+                </Row>
+
+            </Container>
+
+        )
+
+    }
+
+    return (
+        <Dashboard leagueId={leagueId._id} userId={userId} username={username} />
+    )
+
+}
+
+export default PreDashboard;
